06b8dae2c1d7370118006d275edc8f931d47f00d
Johan Kämpe <kampe.johan@gmail.com>
2018-03-18 11:22:37 +0100

    db_mov_tools: add script name to printouts

--- db_mov_tools.py
+++ db_mov_tools.py
@@ -3,11 +3,26 @@ import paths, json, os
 import db_mov as movie_database
 import filetools as ftool
 import movie as mtool
+from printout import print_blue, print_no_line, print_color_between
+from printout import print_script_name as psn
+from printout import print_color_between as pcb
 from printout import print_error, print_warning
+
+def print_log(string, category=None):
+    script = os.path.basename(__file__)
+    psn(script, "", endl=False) # Print script name
+    if category == "error":
+        print_error("Error: ", endl=False)
+    if category == "warning":
+        print_warning("Warning: ", endl=False)
+    if string.find('[') >= 0 and string.find(']') > 0:
+        pcb(string, "blue")
+    else:
+        print(string)
 
 db = movie_database.database()
 if not db.load_success():
-    print("Database read error, quitting...")
+    print_log("database read error, quitting...", category="error")
     quit()
 
 mov_root = mtool.root_path()
@@ -18,7 +33,7 @@ def nfo_imdb_from_omdb_data(mov):
     let = db.movie_data(mov,'letter')
     path = os.path.join(mov_root, let, mov)
     if not imdbid_omdb:
-        print_warning("No imdb omdb-data for {}".format(mov))
+        print_log("no imdb omdb-data for {}".format(mov), category="warning")
         return False
     if imdbid_omdb and ftool.create_nfo(path, imdbid_omdb):
         db.update(mov, 'nfo', True)
@@ -32,7 +47,7 @@ def update_omdb_search(mov):
         db.update(mov, 'omdb', omdb_data)
         return True
     if 'Error' in omdb_data:
-        print_warning("OMDb not found: {}".format(mov))
+        print_log("OMDb not found: {}".format(mov), category="warning")
     return False
 
 def db_maintainance():
@@ -43,17 +58,17 @@ def db_maintainance():
                 print("Added nfo for {}".format(mov))
                 need_save = True
             else:
-                print_error("Could not add nfo for {}".format(mov))
+                print_log("Could not add nfo for {}".format(mov), category="error")
 
         data = db.movie_data(mov, 'omdb')
         if not data or 'Error' in data:
             if update_omdb_search(mov):
-                print("Added omdb for {}".format(mov))
+                print("added omdb for {}".format(mov))
                 need_save = True
             else:
-                print_error("Could not add omdb for {}".format(mov))
+                print_log("could not add omdb for {}".format(mov), category="error")
 
     if need_save:
         db.save()
     else:
-        print("db_maintainance: Nothing updated")
+        print_log("nothing updated")
