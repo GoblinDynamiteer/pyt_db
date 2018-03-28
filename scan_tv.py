ca40278f7448660d3dde20419234f5adb6c8c48e
Johan Kämpe <kampe.johan@gmail.com>
2018-03-28 19:39:35 +0200

    scan_tv: add optional argument for max new show limit

--- scan_tv.py
+++ scan_tv.py
@@ -3,8 +3,7 @@ import paths
 import filetools as ftool
 import tvshow as tvtool
 import db_tv
-import os
-import datetime
+import datetime, argparse, os
 from printout import print_class as pr
 
 pr = pr(os.path.basename(__file__))
@@ -51,12 +50,27 @@ def new_show(folder):
     pr.info("added [ {} ] to database!".format(folder))
     db.add(show)
 
+parser = argparse.ArgumentParser(description='tv scanner')
+parser.add_argument('-m', '--max', dest='max', default=None,
+    help='max new shows to scan')
+args = parser.parse_args()
+
 new_show_count = 0
 new_episode_count = 0
+
+try:
+    max_scan = int(args.max)
+    pr.info("will scan max {} new shows".format(max_scan))
+except:
+    max_scan = None
+
 # Scan for new shows...
 for show in shows:
     if show.startswith("@"): # Diskstation
         continue;
+    if max_scan and new_show_count >= max_scan:
+        pr.info("max new show scan limit reached! breaking")
+        break;
     pr.info("scanning [ {} ]".format(show))
     if not db.exists(show):
         new_show(show)
@@ -67,7 +81,8 @@ for show in shows:
         for ep in episodes:
             if not db.has_ep(show, season, ep):
                 new_episode_count += 1
-                episode = new_episode(show, season, ep)
+                sd = db.show_data(show)
+                episode = new_episode(sd, season, ep)
                 db.add_ep(show, season, episode)
 
 pr.info("done scanning!")
